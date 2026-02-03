3b9f88dda29e161a0c85b5637c99e75b3310967e
Geoff Vrijmoet <geofferyv@gmail.com>
2026-02-03 11:38:51 -0500

    chore: add console log for debugging in client API route
    - Added a console log statement to the `route.ts` file in the client API to assist with debugging during development.
    - File changed: `app/api/clients/[id]/route.ts`.
    
    This change aims to facilitate troubleshooting by providing immediate feedback in the console when the API route is accessed.

--- app/api/clients/[id]/route.ts
+++ app/api/clients/[id]/route.ts
@@ -11,6 +11,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log("Hi!");
 
 type ClientLeanResult = Omit<IClient, '_id'> & { _id: Types.ObjectId };
 
